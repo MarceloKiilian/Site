1a3de21849708b8bbc3f67be06f74a5ecc272594
MarceloKiilian <marcelo.kiilian@gmail.com>
2015-05-16 06:02:36 -0700

    insercaoFontes

---
+++ SiteB/SiteB.Business/Estrutura.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Configuration;
+
+namespace SiteB.Business
+{
+    public class Login
+    {
+        private string ConnectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+        public bool Erro { get; set; }
+        public string MensagemErro { get; set; }
+
+        public Login()
+        {
+            this.Erro = false;
+            this.MensagemErro = string.Empty;
+        }
+
+        public Entities.Login AutenticarLogin(string CPF, string Senha)
+        {
+            Entities.Login[] login = ListaUsuario(new Entities.Login(CPF, Senha));
+            Entities.Login users = login.FirstOrDefault();
+
+            if (users == null)
+            {
+                this.Erro = true;
+                this.MensagemErro = "Usuário ou Senha inválidos!";
+            }
+
+            return users;
+        }
+
+        public bool LoginCadastrado(string Login)
+        {
+            Entities.Login[] login = ListaUsuario(new Entities.Login(CPF));
+            Entities.Login users = login.FirstOrDefault();
+
+            bool existe = users != null && users.Id > 0;
+
+            return existe;
+        }
+
+        public Entities.Login[] ListaUsuario()
+        {
+            return ListaUsuario(null);
+        }
+
+
+        public Entities.Login[] ListaUsuario(Entities.Login login)
+        {
+            List<Entities.Login> lstLogin = new List<Entities.Login>();
+
+            Dados.Connection connection = new Dados.Connection(this.ConnectionString);
+
+
+            connection.AbrirConexao();
+
+            StringBuilder sqlString = new StringBuilder();
+            sqlString.AppendLine("SELECT * FROM SiteBLogin");
+
+            if (login != null)
+            {
+                sqlString.AppendLine("WHERE 1 = 1");
+
+                if (login.Id > 0)
+                {
+                    sqlString.AppendLine("AND Id = " + login.Id + "");
+                }
+
+
+                if (!string.IsNullOrEmpty(login.Senha) && login.Senha.Length > 0)
+                {
+                    sqlString.AppendLine("AND SENHA = '" + login.Senha + "'");
+                }
+
+            }
+        }
+    }
+}
---
+++ SiteB/SiteB.Entities/Estrutura.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteB.Entities
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string CPF { get; set; }
+        public string Perfil { get; set; }
+        public DateTime DtCriacao { get; set; }
+        public bool Ativo { get; set; }
+
+        public Login() { }
+
+        public Login(string CPF)
+        {
+            this.CPF = CPF;
+        }
+
+        public Login(string CPF, string Senha)
+        {
+            this.CPF = CPF;
+            this.Senha = Senha;
+        }
+
+        public Login(int Id, string Nome, string Email, string Senha, string CPF, string Perfil, DateTime DtCriacao, bool Ativo)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Email = Email;
+            this.Senha = Senha;
+            this.CPF = CPF;
+            this.Perfil = Perfil;
+            this.DtCriacao = DtCriacao;
+            this.Ativo = Ativo;
+        }
+    }
+}
